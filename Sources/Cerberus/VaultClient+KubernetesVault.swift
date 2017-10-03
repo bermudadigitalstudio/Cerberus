e236aaa9b7578c95e4cfcf28dc63ef3b2f781124
Thomas Catterall <me@swizzlr.co>
2017-10-03 11:07:03 -0400

    Logging after autologin.

--- Sources/Cerberus/VaultClient+KubernetesVault.swift
+++ Sources/Cerberus/VaultClient+KubernetesVault.swift
@@ -5,7 +5,7 @@ enum KubernetesVaultError: Error {
   case ParseJSONError
 }
 extension VaultClient {
-  public static func autologin(credentialDirectory: String = "/var/run/secrets/boostport.com") throws -> VaultClient {
+  public static func autologin(credentialDirectory: String = "/var/run/secrets/boostport.com", logger: Logger? = nil) throws -> VaultClient {
     guard let vaultTokenData = FileManager().contents(atPath: credentialDirectory + "/vault-token") else {
       throw KubernetesVaultError.CredentialsNotFound
     }
@@ -15,7 +15,7 @@ extension VaultClient {
     guard let authority = json["vaultAddr"] as? String, let url = URL(string: authority), let token = json["clientToken"] as? String else {
       throw KubernetesVaultError.ParseJSONError
     }
-    let client = VaultClient(vaultAuthority: url)
+    let client = VaultClient(vaultAuthority: url, logger: logger)
     client.token = token
     try client.enableAutoRenew()
     return client
